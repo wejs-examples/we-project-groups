dbef0fc922710506e69e736d5a52a05af3ef8bfe
albertosouza <alberto.souza.99@gmail.com>
2016-02-12 18:56:11 -0200

    update home page

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -8,9 +8,9 @@ module.exports = {
 
     if (!res.locals.metadata) res.locals.metadata = {};
 
-    res.locals.metadata.description = 'Host multiple events in one installation and every event have its own content, administrative interface and compatible with We.js plugins';
+    res.locals.metadata.description = 'Host your company social network with groups, categories and contents';
     res.locals.metadata.image = 'http://wejs.org/public/plugin/we-core/files/images/logo.png';
-    res.locals.metadata.keywords = 'Events, conferences, we.js, node.js, javascript';
+    res.locals.metadata.keywords = 'groups, social network, we.js, node.js, javascript';
 
     res.view({ title: we.config.appName });
     // res.goTo('/post');
