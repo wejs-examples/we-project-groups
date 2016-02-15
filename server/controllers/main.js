703f0f191a27dc7bb59208f602536d749e58558f
albertosouza <alberto.souza.99@gmail.com>
2016-02-15 21:14:03 -0200

    update home page metatags

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -6,14 +6,24 @@ module.exports = {
   index: function(req, res) {
     var we = req.getWe();
 
-    if (!res.locals.metadata) res.locals.metadata = {};
+    res.locals.metatagHandler = function metatagHandler(req, res, next) {
 
-    res.locals.metadata.description = 'Host your company social network with groups, categories and contents';
-    res.locals.metadata.image = 'http://wejs.org/public/plugin/we-core/files/images/logo.png';
-    res.locals.metadata.keywords = 'groups, social network, we.js, node.js, javascript';
+      var hostname = req.we.config.hostname;
+
+      res.locals.metatag +=
+        '<meta property="og:url" content="'+hostname+'" />'+
+        '<meta property="og:title" content="We.js groups plugin" />' +
+        '<meta property="og:site_name" content="We.js framework" />'+
+        '<meta property="og:type" content="profile" />'+
+        '<meta property="og:description" content="'+
+          'Host your company social network with groups, categories and contents'+
+        '" />'+
+        '<meta property="og:image" content="http://wejs.org/public/plugin/we-core/files/images/logo.png" />';
+
+      next();
+    };
 
     res.view({ title: we.config.appName });
-    // res.goTo('/post');
   }
 
 };
