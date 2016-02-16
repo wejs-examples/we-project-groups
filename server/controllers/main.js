b4cb39925175e005a4b59e2e5955f290c08c0209
albertosouza <alberto.souza.99@gmail.com>
2016-02-15 22:23:08 -0200

    update home page metatags

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -6,22 +6,17 @@ module.exports = {
   index: function(req, res) {
     var we = req.getWe();
 
-    res.locals.metatagHandler = function metatagHandler(req, res, next) {
+    var hostname = req.we.config.hostname;
 
-      var hostname = req.we.config.hostname;
-
-      res.locals.metatag +=
-        '<meta property="og:url" content="'+hostname+'" />'+
-        '<meta property="og:title" content="We.js groups plugin" />' +
-        '<meta property="og:site_name" content="We.js framework" />'+
-        '<meta property="og:type" content="profile" />'+
-        '<meta property="og:description" content="'+
-          'Host your company social network with groups, categories and contents'+
-        '" />'+
-        '<meta property="og:image" content="http://wejs.org/public/plugin/we-core/files/images/logo.png" />';
-
-      next();
-    };
+    res.locals.metatag +=
+      '<meta property="og:url" content="'+hostname+'" />'+
+      '<meta property="og:title" content="We.js groups plugin" />' +
+      '<meta property="og:site_name" content="We.js framework" />'+
+      '<meta property="og:type" content="profile" />'+
+      '<meta property="og:description" content="'+
+        'Host your company social network with groups, categories and contents'+
+      '" />'+
+      '<meta property="og:image" content="http://wejs.org/public/plugin/we-core/files/images/logo.png" />';
 
     res.view({ title: we.config.appName });
   }
