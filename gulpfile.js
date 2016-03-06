6256f54d75192667b2be4f69dc3cc4bde869f3ab
albertosouza <alberto.souza.99@gmail.com>
2016-03-06 03:30:11 -0300

    update we-gulp-tasks and build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var we = require('we-core');
+var We = require('we-core');
 var projectFolder = process.cwd();
 var gulp = require('gulp');
 var weGulpTasks = require('we-gulp-tasks-default');
+var we = new We();
 
 weGulpTasks(we, gulp, projectFolder, function doneTask() {
   we.exit(function(){
