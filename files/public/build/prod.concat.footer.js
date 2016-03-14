21c13d47ad8d2443815e344970f5439a8afb268d
albertosouza <alberto.souza.99@gmail.com>
2016-03-14 00:56:26 -0300

    update modules and build

--- files/public/build/prod.concat.footer.js
+++ files/public/build/prod.concat.footer.js
@@ -51003,8 +51003,12 @@ we.structure = {
   setForDataValuesWithVisibility: function(formData) {
     switch(formData.visibility) {
       case 'in-page':
-        formData.modelName = we.config.modelName;
-        formData.modelId = we.config.modelId;
+        if (we.config.modelName && we.config.modelId) {
+          formData.modelName = we.config.modelName;
+          formData.modelId = we.config.modelId;
+        } else {
+          formData.path = location.pathname;
+        }
         break;
       case 'in-session':
         formData.modelName = we.config.modelName;
@@ -51055,6 +51059,8 @@ we.structure = {
     modal.find('.steps-body .step1').show();
   },
   goToStep2: function goToStep2() {
+    var self = this;
+
     var modal = $(we.structure.addWidgetModalFormId);
     var regionWidgetsTag = $('#region-'+ this.newWidgetObj.regionName +'-widgets');
 
@@ -51085,20 +51091,24 @@ we.structure = {
           formData[d.name] = d.value;
         });
 
-        var url = we.config.structure.widgetCreateUrl;
-        if (!url) url = '/api/v1/widget';
-        url+='?responseType=json';
-
         we.structure.setForDataValuesWithVisibility(formData);
 
-        $.post(url, formData)
-        .then(function (r) {
+        formData.theme = self.newWidgetObj.theme;
+        formData.regionName = self.newWidgetObj.regionName;
+        formData.context = we.config.widgetContext;
+
+        $.ajax({
+          headers: { 'we-widget-action': 'add' },
+          url: location.pathname+'?responseType=json',
+          method: 'POST',
+          data: {
+            widget: JSON.stringify(formData)
+          }
+        }).then(function (r) {
           // insert after regions actions
           regionWidgetsTag.prepend(r.widget.html);
-        }).always(function(){
+        }).always(function() {
           modal.modal('hide');
-
-          modal.find('form').off( event );
         });
       });
     });
@@ -51121,51 +51131,42 @@ we.structure = {
 
       modalForm.find('form').submit(function( event ) {
         event.preventDefault();
-        var formData = {};
+        var formData = { id: id };
 
         modalForm.find('form').serializeArray().forEach(function (d) {
           formData[d.name] = d.value;
         });
 
-        var url = '/api/v1/widget/'+id+'?responseType=json';
-        if (we.config.structure.widgetUpdateUrl) {
-          url = we.config.structure.widgetUpdateUrl+id+'?responseType=json'
-        }
-
         we.structure.setForDataValuesWithVisibility(formData);
 
         $.ajax({
-          url: url,
+          headers: { 'we-widget-action': 'update' },
+          url: location.pathname+'?responseType=json',
           method: 'POST',
-          dataType: 'json',
-          contentType: 'application/json; charset=utf-8',
-          data: JSON.stringify(formData)
+          data: {
+            widget: JSON.stringify(formData)
+          }
         }).then(function (r) {
           we.events.emit('model-update', 'widget', r.widget);
           widgetTag.after(r.widget.html);
           widgetTag.remove();
         }).always(function(){
           modalForm.modal('hide');
-        })
+        });
       });
     });
   },
   deleteWidget: function deleteWidget(id) {
     if (!id) return console.warn('data-id attribute is required for deleteWidget');
 
-    var url;
-    if (we.config.structure.widgetDeleteUrl) {
-      url = we.config.structure.widgetDeleteUrl+id
-    } else {
-      url = '/api/v1/widget/'+id;
-    }
-    url += '/delete?responseType=json';
-
     if (confirm(we.config.structure.deleteWidgetConfirm)) {
       $.ajax({
-        url: url,
+        headers: { 'we-widget-action': 'delete' },
+        url: location.pathname+'?responseType=json',
         method: 'POST',
-        contentType: 'application/json; charset=utf-8'
+        data: {
+          widgetId: id
+        }
       }).then(function (r) {
         we.events.emit('model-update-after', 'widget', r);
         $('#widget-'+id).remove();
@@ -51178,15 +51179,25 @@ we.structure = {
     if (!modal) throw new Error('sort widget modal not found!', we.structure.sortWidgetModalFormId);
    modal.modal('show');
 
-    var url = '/api/v1/widget-sort/';
-    if (we.config.structure.widgetSortUrl)
-      url = we.config.structure.widgetSortUrl;
+    var url = location.pathname;
 
-    url += we.config.theme + '/'+
-      $('#we-layout').attr('data-we-layout')+
-      '/'+regionName  + '?skipHTML=true&context='+
-      $('#we-layout').attr('data-we-widgetcontext');
-    $.get(url).then(function (f) {
+    $.ajax({
+      headers: { 'we-widget-action': 'find' },
+      url: url+'?responseType=modal',
+      method: 'POST',
+      data: {
+        responseType: 'modal',
+        skipHTML: true,
+        widget: JSON.stringify({
+          theme: we.config.theme,
+          regionName: regionName,
+          layout: $('#we-layout').attr('data-we-layout'),
+          context: $('#we-layout').attr('data-we-widgetcontext'),
+          modelName: we.config.modelName,
+          modelId: we.config.modelId
+        })
+      }
+    }).then(function (f) {
       modal.find('.modal-body').html(f);
     });
   }
@@ -51278,7 +51289,7 @@ we.admin.layouts = {
     var sortableList = $(selector);
     // Sortable rows
     sortableList.sortable({
-      update: function( event, ui ) {
+      update: function updateSort() {
         saveOrder(this);
       }
     });
@@ -51294,20 +51305,19 @@ we.admin.layouts = {
         $(list[i]).attr('data-weight', i);
       }
 
-      var url = '/api/v1/widget-sort/';
-      if (we.config.structure.widgetSortUrl)
-        url = we.config.structure.widgetSortUrl;
-
-      url += we.config.theme + '/'+
-        $('#we-layout').attr('data-we-layout')+
-        '/'+regionName + '?skipHTML=true&responseType=JSON&context='+$('#we-layout').attr('data-we-widgetcontext');
-
       $.ajax({
-        url: url,
+        headers: { 'we-widget-action': 'updateSort' },
+        url: location.pathname+'?responseType=json',
         method: 'POST',
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify({ widgets: widgets })
+        // dataType: 'json',
+        // contentType: 'application/json; charset=utf-8',
+        data: {
+          params: JSON.stringify({
+            regionName: regionName,
+            layout: $('#we-layout').attr('data-we-layout')
+          }),
+          widgets: JSON.stringify(widgets)
+        }
       }).done(function(r) {
         var region = $('#region-'+regionName);
         var widget;
@@ -51321,8 +51331,6 @@ we.admin.layouts = {
           }
           lastWidget = widget;
         }
-
-        // console.log('Done sort widgets', r);
       });
     }
   }
@@ -52377,6 +52385,7 @@ we.notification = {
   count: 0,
   countDisplay: null,
   link: null,
+  notificationsCountCheckDelay: 60000,// check every 1 min
 
   init: function() {
     this.countDisplay = $('.main-menu-link-notification-count');
@@ -52385,7 +52394,7 @@ we.notification = {
     if (this.countDisplay && this.link) this.getNewNotificationCount();
   },
 
-  notificationsCountCheckDelay: 60000,// check every 1 min
+
   lastCheckData: null,
   registerNewCheck: function registerNewCheck() {
     setTimeout(
@@ -52399,16 +52408,20 @@ we.notification = {
 
     $.ajax({
       url: '/api/v1/current-user/notification-count'
-    }).then(function (data){
+    }).then(function onSuccessGetNewNotifications(data) {
       self.count = Number(data.count);
 
       self.updateNotificationsDisplay();
       // update last check time
       self.lastCheckData = new Date().toISOString();
-    }).fail(function (err) {
-      console.error('we.notification.js:error in getNewNotificationCount:', err);
-    }).always(function () {
+
       self.registerNewCheck();
+    }).fail(function afterFailtInGetNewNotifications(err) {
+      // skip new checks if user is unAuthenticated
+      if (err.status != '403') {
+        console.error('we.notification.js:error in getNewNotificationCount:', err);
+        self.registerNewCheck();
+      }
     });
   },
 
